84cdf6957a587a1da4b86a997355f353c564a1d1
izaki93 <ibrahim.mohsen.zaki@gmail.com>
2019-12-15 12:38:40 +0200

    Add Default Profile

--- src/main/java/com/springframework/study/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/springframework/study/services/PrimaryGermanGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("de")
 @Primary
-@Profile({"en","default"})
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryGermanGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello Primary Greeting Service";
+        return "Primärer Begrüßungsdienst";
     }
 }
