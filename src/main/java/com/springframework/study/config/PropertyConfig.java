486b1b1edd1d4d1dd3d00b31e1843d80cb94313b
izaki93 <ibrahim.mohsen.zaki@gmail.com>
2019-12-18 16:59:14 +0200

    adding environment varibales properties.

--- src/main/java/com/springframework/study/config/PropertyConfig.java
+++ src/main/java/com/springframework/study/config/PropertyConfig.java
@@ -1,16 +1,20 @@
 package com.springframework.study.config;
 
 import com.springframework.study.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
 
+    @Autowired
+    Environment environment;
     @Value("${ds.username}")
     String user;
 
@@ -23,7 +27,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(environment.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
